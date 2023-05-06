ad536a9438f2c9ed29f91656ec35f8199e6f7b10
gelemou123 <gelemou@163.com>
2023-05-06 18:36:10 +0800

    5.6 首页新闻中心

---
+++ pages/components/newsCenter/newsCenter.js
@@ -0,0 +1,41 @@
+// pages/componets/newsCenter/newsCenter.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    news:[{
+      newsId: 1,
+      newsTitle: "阿萨法沙发",
+      newsTime: "2022-12-01",
+      imageSrc: "../../../public/0001.png"
+    },
+    {
+      newsId: 2,
+      newsTitle: "阿萨法沙发2",
+      newsTime: "2022-12-01",
+      imageSrc: "../../../public/0001.png"
+    },
+    {
+      newsId: 3,
+      newsTitle: "阿萨法沙发3",
+      newsTime: "2022-12-01",
+      imageSrc: "../../../public/0001.png"
+    },
+  ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
