e8bb1d330a349f5dd0c8c9facdd690105fab5e28
gelemou123 <gelemou@163.com>
2023-05-09 08:08:29 +0800

    轮播图组件

---
+++ pages/components/carousel/carousel.js
@@ -0,0 +1,23 @@
+// pages/components/carousel/carousel.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    background: ['/public/蒙版组 1.png', '/public/0001.png', '/public/0002.png'],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
