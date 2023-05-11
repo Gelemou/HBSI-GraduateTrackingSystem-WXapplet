98af885d312c528bd33afc1cb9ac09d5fbd47c22
gelemou <gelemou@163.com>
2023-05-11 08:57:31 +0800

    dynatic

--- pages/components/active/active.js
+++ pages/components/active/active.js
@@ -1,4 +1,5 @@
 // pages/components/active/active.js
+const http = require("../../../utils/request");
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +12,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    activity: [],
   },
-
+  created:function() {
+    let that = this;
+    http.request("/activity/getListAll", {pageNum: 1, pageSize: 4}).then((res) => {
+      that.setData({ activity: res.result.list});
+    })
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onSwitch() {
+      wx.navigateTo({
+        url: '/pages/components/second/activeDetail/activeDetail',
+      })
+    }
   }
 })
--- pages/components/notice/notice.js
+++ pages/components/notice/notice.js
@@ -1,4 +1,5 @@
 // pages/componets/newsCenter/newsCenter.js
+const http = require("../../../utils/request")
 Component({
   /**
    * 组件的属性列表
@@ -31,11 +32,24 @@ Component({
     },
   ]
   },
-
+  // 获取接口数据
+  created:function() {
+    let that = this;
+    http.request("/news/wx/getListAll", {
+      pageNum: 1, pageSize: 3, newsType: 1
+    }).then((res) => {
+      that.setData({notice: res.result.list});
+    })
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    // 跳转到二级页面
+    onSwitch() {
+      wx.navigateTo({
+        url: '/pages/components/second/noticeDetail/noticeDetail',
+      })
+    }
   }
 })
